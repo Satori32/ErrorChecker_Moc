5cdf6e3ab352c4e8c9d4edd78b65f559736195f3
Satori32 <Huga8989+githubf@gmail.com>
2024-11-09 12:09:18 +0900

    2nd

--- ソース.cpp
+++ ソース.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <time.h>
 
 #include "Vector.h"
 
@@ -19,6 +20,20 @@ struct Bit {
 	T B;//i think to normaly use is use type of bool.
 };
 
+struct SomeFileSystem {
+	void* F = NULL;
+}
+
+struct SomeCompressonSystem {
+	void* C = NULL;
+}
+struct Ticket {
+	void* T = NULL;
+};
+struct Handle {
+	void* H = NULL;
+};
+
 struct ErrorChecher {
 	void* Seed = NULL;
 	//type T = Null;
@@ -32,11 +47,16 @@ struct ErrorChecher {
 	//type A = AA;
 	A Memo;
 
+	Handle System;
+	Ticket Huda;
+
 	Bit Mind;
 	//type W = unsigned char ;
 	typedef unsigned char W;
 	//type Word = W;
-	Word Strage;//are you the stateful?
+	Word Strage;//are you the stateful? and are you need SomeFileSystem. and are you need SomeCompressionSystem.
+	//Word Pamission;
+	//Word Chash;
 
 	R(*MakeSequence)(N& A, N& B)=NULL;
 	R(*SATU)(N& A, N& B) = NULL;
@@ -59,6 +79,136 @@ ErrorChecher ConstructErrorChecker(R(*MakeSequence)(N& A, N& B), R(*SATU)(N& A, 
 	return E;
 }
 
+bool OpenKABARU(ErrorChecher& In, Ticket& T) {
+	Bit KABARU;
+	//do it.
+	return KABARU;
+}
+bool CloseKABARU(ErrorChecher& In) {
+	Bit KABARU;
+	//do it.
+	return KABARU;
+}
+bool SendHELLOW(ErrorChecher& In) {
+	Bit KABARU;
+	//Do Send "Hellow World!!".
+	return KABARU;
+}
+bool ConnectKABARU(ErrorChecher& In, Bit CA,Ticket& T) {//Ticket is same to Open time.
+	Bit KABARU;
+	//do it.
+	return KABARU;
+}
+bool IsArraiveConnectToKABARU(ErrorChecher& In) {
+	Bit KABARU;
+	//do it.
+	return KABARU;
+}
+bool WriteToKABARU(ErrorChecher In, In.Word* Data, intmax_t L) {
+	Bit KABARU;
+	//do it.
+	return KABARU;
+}
+bool IsWriteOKToKABARU(ErrorChecher& In) {
+	Bit KABARU;
+	//do it.
+	return KABARU;
+}
+bool ReadFromKABARU(ErrorChecher& In) {
+	Bit KABARU;
+	//Do it.
+	return KABARU;
+}
+bool SendRequest(ErrorChecher& In, Null& N) {
+	Bit KABARU;
+	//Do Send N?
+	return KABARU;
+}
+Void GetSendRequestAnther(ErrorChecher& In) {
+	Bit KABRU;
+	Void R;
+	//Do Negotiation to Get Anther to user sender.
+}
+bool IsReadOKFromKABARU(ErrorChecher& In) {
+	Bit KABARU;
+	//DO it.
+	return KABARU;
+}
+bool DisConnectByKABARU(ErrorChecher& In) {
+	Bit KABARU;
+	//do it/
+	return KANARU;
+}
+Void MakeWriteFormat(ErrorChecher& In, In.Word* Data, intmax_t L) {
+	Void V = { Data,L };
+	//do it?
+	return V;
+}
+bool SendEndProsessMessage(ErrorChecher& In) {
+	Bit KABARU;
+	Void V = "I am Main Prosess is End.try to Disconnect.";
+	//do Send V;
+	return KABARU;
+}
+bool SendThankYou(ErrorChecher& In) {
+	Bit KABARU;
+	Void V = "Thank You.";
+	//do Send V;
+	return KABARU;
+}
+bool HardwereSleep(ErrorChecher& In, clock_t C) {
+	Bit Ok;
+	//do local hardwere Sleep to C.C by spec of time.
+	return Ok;
+}
+bool ConnectFlow(ErrorChecher& In) {
+	Bit B;
+	Bit KABARU;
+	Null N;
+	Void V;
+	if (OpenKABARU(In, In.Huda) == false) { return B; }
+	if (ConnectKABARU(In, B, In.Huda) == false) { CloseKABARU(In); return{ B, KABARU}; }
+	if (IsArraiveConnectToKABARU(In) == false) { CloseKABARU(In); return { B,KABARU }; }
+	if (SendHELLOW(In) == false) { DisConnectByKABARU(In); CloseKABARU(In); return { B,KABARU }: }
+	if (IsArraiveConnectToKABARU(In) == false) { DisConnectByKABARU(In); CloseKABARU(In); return{ B,KABARU }; }
+	Void X;
+	if (SendRequest(In, N) == false) { X = GetSendRequestAnther(In); DisConnectByKABARU(In); CloseKABARU(In); return { B,KABARU,X }; }
+	X = GetSendRequestAnther(In);//if reason is negative. i prosess to convert to false.
+
+	if (X == false) { DisConnectByKABARU(In); CloseKABARU(); return { B,KABARU,X }l; }
+
+	do
+	{
+		if (IsArraiveConnectToKABARU(In) == false) { break; }
+		if (X == "Read") {
+			while (IsReadOKFromKABARU(In) == false) {
+				ReadFromKABARU(In);
+			}
+		}
+		if (X == "Write") {
+			while (IsWriteOKToKABARU(In) == false) {
+				In.Word W[2] = { V,N };
+				WriteToKABARU(In, W, sizeof(W));
+			}
+		}
+
+		if (SendRequest(In, N) != false) { X = GetSendRequestAnther(In); };
+
+	} while (X!="EndOfStream");
+
+	do {
+		HardwereSleep(In, 3000 / CLOCKS_PER_SEC);
+		SendEndProsessMessage(In);
+		X = GetSendRequestAnther(In);
+		B += X;
+	} while (X == "Wait");//this loops end flag is nearly "Wait".
+
+	SendThankYou(In);
+	DisConnectByKABARU(In);
+	CloseKABARU(In);
+
+	return { B,KABARU };
+}
 bool Free(ErrorChecher& In) {
 	//do it.
 	Free(In.Sequence);
@@ -153,6 +303,8 @@ bool IsComplete(ErrorChecher& In, Void& Box, Null& hako) {
 bool Flow(ErrorChecher& In, Void& Box, Null& hako) {//one of usecase. to other, i have lambda in structuer.
 
 	Bit B;
+
+	if (ConnectFlow(In) == false) {//Shal I not use Far system? if no to return B;}
 
 	Initialize(In, Box, hako);//need think is many and some.
 	for (size_t i = Box; i < hako; i++) {
